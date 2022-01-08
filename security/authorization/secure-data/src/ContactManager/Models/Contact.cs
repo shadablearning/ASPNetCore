bc4b8097d1f0342d572057c90bb63e4056deaf98
shadablearning <51901767+shadablearning@users.noreply.github.com>
2022-01-08 17:17:34 +0530

    Add UderId and Contact Status in Conatct Model. OwnerID is the user's ID from the AspNetUser table in the Identity database. The Status field determines if a contact is viewable by general users. Also Add Imp Links Doc

--- security/authorization/secure-data/src/ContactManager/Models/Contact.cs
+++ security/authorization/secure-data/src/ContactManager/Models/Contact.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace ContactManager.Models
 {
+    //Create a new migration and update the database for new properties
     public class Contact
     {
         public int ContactId { get; set; }
+
+        // OwnerID is the user's ID from the AspNetUser table in the Identity database
+        public string? OwnerID { get; set; }
+
         public string? Name { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
@@ -16,5 +21,14 @@ namespace ContactManager.Models
         public string? Zip { get; set; }
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+
+        public ContactStatus Status { get; set; }
+    }
+    public enum ContactStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
     }
 }
+
